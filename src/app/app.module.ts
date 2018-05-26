24d3bf7674fd63fb6b9d22ebdd10d6cd12b25a3d
Nuntawut Kaoungku <b5111299@gmail.com>
2018-05-26 21:17:49 +0700

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
-//import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LocationTrackerProvider } from '../providers/location-tracker/location-tracker';
 
 @NgModule({
   declarations: [
@@ -27,8 +28,9 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     BackgroundGeolocation,
-    //Geolocation,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocationTrackerProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse  } from '@ionic-native/background-geolocation';
-//import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import { LocationTrackerProvider } from '../../providers/location-tracker/location-tracker';
 
 @Component({
   selector: 'page-home',
@@ -12,42 +11,16 @@ export class HomePage {
   public lat: number = 0;
   public lng: number = 0;
   
-  constructor(private backgroundGeolocation: BackgroundGeolocation, public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public locationTracker: LocationTrackerProvider) {
 
   }
 
-  startTracking(){
-
-    let config: BackgroundGeolocationConfig = {
-      desiredAccuracy: 10,
-      stationaryRadius: 20,
-      distanceFilter: 30,
-      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-      stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-    };
-
-    this.backgroundGeolocation.configure(config).subscribe((location: BackgroundGeolocationResponse) => {
- 
-      console.log('BackgroundGeolocation:  ',location);
-   
-      this.lat = location.latitude;
-      this.lng = location.longitude;
-   
-    }, (err) => {
-   
-      console.log(err);
-   
-    });
-
-    this.backgroundGeolocation.start();
+  start(){
+    this.locationTracker.startTracking();
   }
-
-  stopTracking() {
  
-    console.log('stopTracking');
-   
-    this.backgroundGeolocation.stop();
-   
+  stop(){
+    this.locationTracker.stopTracking();
   }
 
 }
