feea8b8f138a14e980ef13b3c2cfba1cd82ec626
Nuntawut Kaoungku <b5111299@gmail.com>
2018-05-28 22:28:24 +0700

    get data from angularfire2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationTrackerProvider } from '../../providers/location-tracker/location-tracker';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +10,16 @@ import { LocationTrackerProvider } from '../../providers/location-tracker/locati
 })
 export class HomePage {
 
-  public lat: number = 0;
-  public lng: number = 0;
-  
-  constructor(public navCtrl: NavController,public locationTracker: LocationTrackerProvider) {
+  public fire_lat: number = 0;
+  public fire_lng: number = 0;
 
+  item: Observable<any>;
+  
+  constructor(public navCtrl: NavController,public locationTracker: LocationTrackerProvider,public db: AngularFireDatabase) {
+    this.db.object('Bus_1').valueChanges().subscribe(data=>{
+      this.fire_lat = data['lat'];
+      this.fire_lng = data['lng'];
+    });
   }
 
   start(){
